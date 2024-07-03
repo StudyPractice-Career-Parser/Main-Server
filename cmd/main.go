de4af404a9b8b4fd6223302f12cb1dbf59198d72
DaNKAadnka <danka2202@yandex.ru>
2024-07-03 23:21:30 +0800

    Config added

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,25 @@ import (
 	handler "main-server/pkg/handlers"
 	"main-server/pkg/repository"
 	"main-server/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error occured while reading config")
+	}
 	repos := repository.NewRepository()
 	service := service.NewService(repos)
 	handlers := handler.NewHandler(service)
 	srv := new(parser.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured")
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured when server runs")
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
